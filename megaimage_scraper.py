f4c3f396d396abe4e60ea8c72c9277beb4d695ed
AndreiDobrin <andrei.dobrin901@gmail.com>
2025-12-30 15:51:40 +0200

    headless

--- megaimage_scraper.py
+++ megaimage_scraper.py
@@ -80,7 +80,7 @@ while True:
 
             # DRIVER OPTIONS
             chrome_options = Options()
-            #chrome_options.add_argument("--headless") 
+            chrome_options.add_argument("--headless") 
             chrome_options.add_argument("--disable-dev-shm-usage") 
             chrome_options.add_argument("--no-sandbox") 
             chrome_options.add_argument("--window-size=1920,1080")
