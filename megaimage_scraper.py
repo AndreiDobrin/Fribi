5b6d83a193e2f0b60ab50aeab393d244fcea18b8
AndreiDobrin <andrei.dobrin901@gmail.com>
2025-12-30 15:48:21 +0200

    memory issues

--- megaimage_scraper.py
+++ megaimage_scraper.py
@@ -80,12 +80,19 @@ while True:
 
             # DRIVER OPTIONS
             chrome_options = Options()
-            chrome_options.add_argument("--headless") 
+            #chrome_options.add_argument("--headless") 
             chrome_options.add_argument("--disable-dev-shm-usage") 
             chrome_options.add_argument("--no-sandbox") 
             chrome_options.add_argument("--window-size=1920,1080")
             chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--single-process")
+            
+                    # BLOCK IMAGES & CSS
+            prefs = {
+                "profile.managed_default_content_settings.images": 2, 
+                "profile.managed_default_content_settings.stylesheets": 2,
+                "profile.managed_default_content_settings.fonts": 2
+            }
+            chrome_options.add_experimental_option("prefs", prefs)
 
             driver = webdriver.Chrome(options=chrome_options)
             wait = WebDriverWait(driver, 10)
